49ef2fcf22045814d6e8f32c3df502ef65024895
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2023-03-31 02:46:42 +0100

    test: example using root module

--- examples/default/versions.tf
+++ examples/default/versions.tf
@@ -3,12 +3,12 @@
 ##############################################################################
 
 terraform {
-  required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = "~>1.43.0"
-    }
-  }
+  #  required_providers {
+  #    ibm = {
+  #      source  = "IBM-Cloud/ibm"
+  #      version = "~>1.43.0"
+  #    }
+  #  }
   required_version = ">=1.3"
 }
 
