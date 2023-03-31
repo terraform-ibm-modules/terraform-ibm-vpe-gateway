3ba705fdb88a5d5527f156d43bc447b61f50afa1
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2023-03-31 10:31:19 +0100

    test: example using root module

--- examples/default/variables.tf
+++ examples/default/variables.tf
@@ -42,7 +42,7 @@ variable "subnet_zone_list" {
 variable "create_vpc" {
   description = "Create a VPC instance."
   type        = bool
-  default     = false
+  default     = true
 }
 
 ##############################################################################
