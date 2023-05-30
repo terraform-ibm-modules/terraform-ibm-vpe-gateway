581590a144b786dc50cb7b0f9a03b46ae393ffd4
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2023-05-30 21:33:43 +0000

    adds output

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,8 +1,3 @@
-output "vpe_endpoints" {
-  description = "Vpe Service Endpoints"
-  value       = module.vpes.vpe_service_endpoints
-}
-
 output "vpe_ips" {
   description = "The endpoint gateway reserved ips"
   value       = module.vpes.vpe_ips
