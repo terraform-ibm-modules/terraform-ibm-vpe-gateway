18b01c3eda1f677933358134689406f5f691a56e
mounika-nalla <mounika.nalla@ibm.com>
2023-06-25 23:40:22 -0400

    fix: resolve conflict

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -66,7 +66,7 @@ module "vpe_security_group" {
 
 module "postgresql_db" {
   source            = "terraform-ibm-modules/icd-postgresql/ibm"
-  version           = "3.3.0"
+  version           = "3.3.1"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-vpe-pg"
   region            = var.region
