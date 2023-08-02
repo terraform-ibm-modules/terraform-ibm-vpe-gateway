030f7825e04d43fcb62f8c189cba35e26f648fc9
Aashiq-J <aashiq.jacob@ibm.com>
2023-08-02 13:35:22 +0530

    change the data structure of the ips output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "vpe_ips" {
   description = "The endpoint gateway reserved ips"
-  value       = [for vpe_pg in ibm_is_virtual_endpoint_gateway.vpe : vpe_pg.ips]
+  value = { for vpe_pg in ibm_is_virtual_endpoint_gateway.vpe :
+  vpe_pg.name => vpe_pg.ips }
 }
 
 output "crn" {
