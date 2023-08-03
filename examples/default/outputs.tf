441a96aee85351f27edaf3ddae4328d40f138f9f
Steve Peggs <peggs@uk.ibm.com>
2023-08-03 08:57:26 +0100

    fix: restore example outputs

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,3 +1,8 @@
+output "vpe_ips" {
+  description = "The endpoint gateway reserved ips"
+  value       = module.vpes.vpe_ips
+}
+
 output "crn" {
   description = "The CRN of the endpoint gateway"
   value       = module.vpes.crn
