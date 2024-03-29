9f063a5601c4110786334e287fd8a151fcaea3ba
Muhammad Arif <CC108-muhammad-arif@CC108-Muhammad-Arif.local>
2024-03-29 18:25:46 +0500

    requested changes done

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -21,7 +21,7 @@ module "vpc" {
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
-  name              = "${var.prefix}-vpc"
+  name              = "vpc-instance"
   tags              = var.resource_tags
 }
 
--- examples/every-mt-vpe/main.tf
+++ examples/every-mt-vpe/main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
-  name              = "vpc"
+  name              = "vpc-instance"
   tags              = var.resource_tags
 }
 
