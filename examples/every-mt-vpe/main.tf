3d2e347174b8e94bf8db96897b677c752342e49c
Muhammad Arif <CC108-muhammad-arif@CC108-Muhammad-Arif.local>
2024-04-04 02:56:49 +0500

    error fixed

--- examples/every-mt-vpe/main.tf
+++ examples/every-mt-vpe/main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
-  name              = "vpc-instance"
+  name              = "vpc"
   tags              = var.resource_tags
 }
 
