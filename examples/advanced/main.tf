0adaf3d880bbdd4969058924528cfb4c2747476e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-08-18 06:39:23 +0000

    chore(deps): update terraform-module

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "vpc" {
   count             = var.vpc_id != null ? 0 : 1
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.18.3"
+  version           = "7.19.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -65,7 +65,7 @@ module "vpe_security_group" {
 
 module "postgresql_db" {
   source            = "terraform-ibm-modules/icd-postgresql/ibm"
-  version           = "3.14.3"
+  version           = "3.16.5"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-vpe-pg"
   region            = var.region
