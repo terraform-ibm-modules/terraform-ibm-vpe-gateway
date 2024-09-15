6b4923ab481522b7bf3b17537093a1d7d456117a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-09-15 13:33:14 +0000

    chore(deps): update terraform terraform-ibm-modules/icd-postgresql/ibm to v3.17.8

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -65,7 +65,7 @@ module "vpe_security_group" {
 
 module "postgresql_db" {
   source            = "terraform-ibm-modules/icd-postgresql/ibm"
-  version           = "3.17.7"
+  version           = "3.17.8"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-vpe-pg"
   region            = var.region
