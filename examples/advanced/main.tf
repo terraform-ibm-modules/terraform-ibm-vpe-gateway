7fa1ed88d2a1eaa800f0d8e4852af6b2762164a2
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-04 16:15:25 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "vpc" {
   count             = var.vpc_id != null ? 0 : 1
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.3.0"
+  version           = "8.5.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -45,7 +45,7 @@ data "ibm_is_security_group" "default_sg" {
 
 module "vpe_security_group" {
   source                       = "terraform-ibm-modules/security-group/ibm"
-  version                      = "2.7.0"
+  version                      = "2.8.0"
   security_group_name          = "${var.prefix}-vpe-sg"
   add_ibm_cloud_internal_rules = false # No need for the internal ibm cloud rules for SG associated with VPEs
 
@@ -65,7 +65,7 @@ module "vpe_security_group" {
 
 module "postgresql_db" {
   source              = "terraform-ibm-modules/icd-postgresql/ibm"
-  version             = "4.2.11"
+  version             = "4.2.17"
   resource_group_id   = module.resource_group.resource_group_id
   name                = "${var.prefix}-vpe-pg"
   region              = var.region
