b04da8c152cff36a29f818f8c79062f4e99324af
shemau <shemau@gmail.com>
2025-10-20 16:38:29 +0100

    fix: Remove VPC as cause of updade issues

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.6.0"
+  version           = "8.3.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
