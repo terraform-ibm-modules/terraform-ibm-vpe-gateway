1b70ba37ec92c7a7666fa39ac088e996abbffb85
shemau <shemau@gmail.com>
2025-11-19 16:57:49 +0000

    fix: Relax regex validation of service CRN (`cloud_service_by_crn`) (#727)

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -127,12 +127,13 @@ variable "cloud_service_by_crn" {
     })
   )
   default = []
+
   validation {
     condition = (
       length(var.cloud_service_by_crn) == 0 ? true : alltrue([
         for service in var.cloud_service_by_crn : can(
           regex(
-            "^crn:v1:[^:]+:[^:]+:[^:]+:[^:]+:a/[0-9a-f]{32}(:[^:]*)*:{0,2}$",
+            "^crn:v\\d(?::(?:(?:[^:]*)(?:[^:]|$))?){8}$",
             service.crn
           )
         )
