e6ad2d6ad077da22ba1c7344b0f521fbff0be7d4
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-24 20:11:42 +0000

    chore(deps): update dependencies (#759)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "vpc" {
   count             = var.vpc_id != null ? 0 : 1
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.12.2"
+  version           = "8.12.3"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -63,7 +63,7 @@ module "vpe_security_group" {
 
 module "postgresql_db" {
   source              = "terraform-ibm-modules/icd-postgresql/ibm"
-  version             = "4.6.7"
+  version             = "4.6.8"
   resource_group_id   = module.resource_group.resource_group_id
   name                = "${var.prefix}-vpe-pg"
   region              = var.region
