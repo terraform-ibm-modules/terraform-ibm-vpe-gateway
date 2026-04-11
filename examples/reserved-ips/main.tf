f67f0b0b727a037b73f2564f737bf964d3a95e43
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-04-11 02:42:30 +0000

    fix(deps): update dependencies

--- examples/reserved-ips/main.tf
+++ examples/reserved-ips/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.8"
+  version = "1.6.0"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -17,7 +17,7 @@ module "resource_group" {
 module "vpc" {
   count             = var.vpc_id != null ? 0 : 1
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.15.11"
+  version           = "8.16.2"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
