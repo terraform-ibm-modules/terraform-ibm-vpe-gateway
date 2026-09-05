8b8b00e36cd47461d371e44a05bbd89b9401f9d8
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-05 08:23:27 +0000

    chore(deps): update dependencies

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "10.0.3"
+  version           = "10.0.4"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
